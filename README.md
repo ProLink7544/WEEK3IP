8ad3fad1d144c3a6fa5ded738b600cd187bb5953
Protus N <114995915+ProLink7544@users.noreply.github.com>
2022-11-19 13:04:20 +0300

    Add files via upload
    changes

---
+++ README.md
@@ -0,0 +1,33 @@
+##### This project is about the prediction of names, Version Date: 20th Oct 2022
+
+#### By **Protus**
+
+## Description
+
+This project picks the user’s birthday and calculates the day of the week they were born and then by gender outputs their Akan names which is a community in Ghana were kids are given names as per the day they were born.
+
+## Setup/Installation Requirements
+
+- The project is easy-to-understand
+
+- Just click on the hyperlinks that will direct you to view Github projects.
+
+## Known Bugs
+
+There are no known bugs on this project.
+
+## Technologies Used
+
+Languages used include HTML,JavaScript and CSS. I have used editor Visual Studio Code for creating the app and Git Bash terminal for coding.
+
+## The live link is below
+
+## Support and contact details
+
+If you need any assistance on any issues or have questions, ideas or concerns. Kindly contact me through email: prolink@gmail.com to make a contribution to the code or for any assistance on the app/project.
+
+## License
+
+_The License used is GPL_
+
+Copyright (c) 2022 **protus ngugi**
---
+++ js/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+    $("#development-image").click(function(){
+      $("#development-image").slideDown('1500').hide('1000');
+      $("#development").show('1500');
+    });
+    $("#development").click(function(){
+      $("#development").slideDown('1500');
+      $("#development-image").slideDown('1500');
+    });
+  });
+  
+  $(document).ready(function(){
+    $("#design-image").click(function(){
+      $("#design-image").slideDown('1500').hide('1000');
+      $("#design").show('1500');
+    });
+    $("#design").click(function(){
+      $("#design").slideDown('1500');
+      $("#design-image").slide('1500');
+    });
+  });
+  
+  $(document).ready(function(){
+    $("#product-image").click(function(){
+      $("#product-image").slideDown('1500').hide('1000');
+      $("#product").show('1500');
+    });
+    $("#product").click(function(){
+      $("#product").slideDown('1500');
+      $("#product-image").slideDown('1500');
+    });
+  });
+  
+  /*an alert display reacting to the button */
+  $(document).ready(function(){
+    $("form#form34A").submit(function(event){
+     event.preventDefault();
+      var name = $("input#MERGE1").val();
+      var email = $("input#MERGE0").val();
+      var message = $("textarea#comment").val();
+      if ($("input#MERGE1").val() && $("input#MERGE0").val()){
+        alert (name + ", we have received your message. Thank you!!!!!.");
+      }
+      else {
+        alert("Please enter a valid name and email!");
+      }
+      
+    });
+  });
